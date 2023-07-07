bf1143fdc1e1c083f6eb1d3973446b31a3441e30
arrkhalila <130886902+arrkhalila@users.noreply.github.com>
2023-07-07 14:33:19 +0700

    Add files via upload

--- src/router/index.js
+++ src/router/index.js
@@ -1,25 +1,21 @@
-import { createRouter, createWebHistory } from 'vue-router'
-import HomeView from '../views/HomeView.vue'
+import { createRouter, createWebHistory } from 'vue-router';
+
+import cuaca from '../components/cuaca.vue';
+import lokasi from '../components/lokasi.vue';
+import waktu from '../components/waktu.vue';
+import foto from '../components/foto.vue';
+import Home from'../components/Home.vue'
 
 const routes = [
-  {
-    path: '/',
-    name: 'home',
-    component: HomeView
-  },
-  {
-    path: '/about',
-    name: 'about',
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
-    component: () => import(/* webpackChunkName: "about" */ '../views/AboutView.vue')
-  }
-]
+  { path: '/weather', component: cuaca },
+  { path: '/location', component: lokasi },
+  { path: '/stopwatch', component: waktu },
+  { path: '/photo', component: foto },
+  {path: '/', component: Home},
+];
 
 const router = createRouter({
-  history: createWebHistory(process.env.BASE_URL),
-  routes
-})
-
-export default router
+  history: createWebHistory(),
+  routes,
+});
+export default router;
