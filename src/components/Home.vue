e9de330f0006d6640189f243d66ee3d99039f006
arrkhalila <130886902+arrkhalila@users.noreply.github.com>
2023-07-07 14:32:29 +0700

    Add files via upload

---
+++ src/components/Home.vue
@@ -0,0 +1,20 @@
+<template>
+    <div>
+    <h2>WELCOME TO WIDGET APP</h2>
+    <p>kindly click on the widget provided above</p>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'App',
+};
+</script>
+
+<style>
+h2 {
+  font-display: center;
+  margin-top: 100px;
+  font-family:'Segoe UI', Tahoma, Geneva, Verdana, sans-serif;
+}
+</style>
---
+++ src/components/lokasi.vue
@@ -0,0 +1,152 @@
+<template>
+    <div class="location-widget">
+      <h2>LOCATION</h2>
+      <div v-if="latitude && longitude">
+        <p>Latitude : {{ latitude }}</p>
+        <p>Longitude : {{ longitude }}</p>
+      </div>
+      <div v-else>
+        <p>Load your location...</p>
+      </div>
+  
+      <div class="location-input">
+        <label for="latitude">Latitude :</label>
+        <input type="text" id="latitude" v-model="inputLatitude" />
+      </div>
+      <div class="location-input">
+        <label for="longitude">Longitude :</label>
+        <input type="text" id="longitude" v-model="inputLongitude" />
+      </div>
+  
+      <br><button @click="fetchLocationDetails">Click for Details</button>
+  
+      <div v-if="foundLocation">
+        <h3>Location Details</h3>
+        <p>{{ foundLocation.components.country }}</p>
+        <p>{{ foundLocation.components.city }}</p>
+        <p>{{ foundLocation.components.street }}</p>
+        <p>Postal Code: {{ foundLocation.components.postcode }}</p>
+      </div>
+    </div>
+  </template>
+  
+  <script>
+  export default {
+    data() {
+      return {
+        latitude: null,
+        longitude: null,
+        inputLatitude: '',
+        inputLongitude: '',
+        foundLocation: null,
+      };
+    },
+    mounted() {
+      if (navigator.geolocation) {
+        navigator.geolocation.getCurrentPosition(this.getPosition);
+      }
+    },
+    methods: {
+      getPosition(position) {
+        this.latitude = position.coords.latitude;
+        this.longitude = position.coords.longitude;
+      },
+      async fetchLocationDetails() {
+        try {
+          const apiKey = '92591005a7b94008909d59a64b6d2a49';
+          const latitude = this.inputLatitude || this.latitude;
+          const longitude = this.inputLongitude || this.longitude;
+          const apiUrl = `https://api.opencagedata.com/geocode/v1/json?q=${encodeURIComponent(
+            latitude + ',' + longitude
+          )}&key=${apiKey}`;
+          const response = await fetch(apiUrl);
+          const data = await response.json();
+          if (data.results && data.results.length > 0) {
+            const location = data.results[0];
+            this.foundLocation = location;
+            console.log('Location:', location);
+
+          }
+        } catch (error) {
+          console.error('Error fetching location data:', error);
+        }
+      },
+    },
+  };
+  </script>
+  
+  <style scoped>
+  .location-widget {
+  border: 1.5px solid #bdbcbc;
+  padding: 20px;
+  margin-bottom: 30px;
+  text-align: center;
+  background-color: #d4e8fc;
+  border-radius: 8px;
+  box-shadow: 0 2px 4px rgba(0, 0, 0, 0.1);
+}
+.location-widget h2 {
+  color: #1d4158;
+  font-size: 24px;
+  margin-bottom: 15px;
+  margin-top: 10px;
+}
+.location-widget p {
+  color: #2c3e63;
+}
+.location-input {
+  margin-top: 20px;
+}
+.location-input label {
+  display: block;
+  margin-bottom: 5px;
+  color: #142639;
+  font-size: 16px;
+}
+.location-input input {
+  width: 200px;
+  padding: 10px;
+  margin-right: 10px;
+  border: 1px solid #ccc;
+  border-radius: 4px;
+  font-size: 16px;
+}
+.location-input button {
+  padding: 10px 20px;
+  background-color: #4caf50;
+  color: white;
+  border: none;
+  border-radius: 4px;
+  font-size: 16px;
+  cursor: pointer;
+  transition: background-color 0.3s ease;
+}
+.location-input button:hover {
+  background-color: #45a049;
+}
+.location-input button:disabled {
+  background-color: #ccc;
+  color: #999;
+  cursor: not-allowed;
+}
+.location-details {
+  margin-top: 20px;
+  text-align: left;
+}
+.location-details h4 {
+  margin-bottom: 10px;
+  color: #333;
+  font-size: 18px;
+}
+.location-details p {
+  margin: 5px 0;
+  color: #9d9d9d;
+}
+.error-message {
+  color: red;
+  margin-top: 10px;
+}
+h2, p{
+  font-family:'Segoe UI', Tahoma, Geneva, Verdana, sans-serif;
+}
+  </style>
